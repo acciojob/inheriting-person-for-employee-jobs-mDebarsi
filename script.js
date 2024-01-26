f8af4352c879a3b2a7e920dad1e9ae363ca3dc99
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2024-01-26 19:05:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add greet method to Person prototype
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Constructor function for Employee
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor with 'this' context
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Add jobGreet method to Employee prototype
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
